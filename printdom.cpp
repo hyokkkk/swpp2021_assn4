d616b3620f89fa2fb58dfc3cf239ea8ad7955952
hyokkkk <tahyok7123@naver.com>
2021-04-14 20:28:51 +0900

    align modified

--- printdom.cpp
+++ printdom.cpp
@@ -18,58 +18,58 @@ using namespace std;
 namespace {
 class PrintDominance : public PassInfoMixin<PrintDominance> {
 public:
-  PreservedAnalyses run(Function &F, FunctionAnalysisManager &FAM) {
-    DominatorTree &DT = FAM.getResult<DominatorTreeAnalysis>(F);
-    for (Function::iterator I = F.begin(), E = F.end(); I != E; ++I) {
-      for (Function::iterator I2 = I; I2 != E; ++I2) {
-        BasicBlock &B1 = *I;
-        BasicBlock &B2 = *I2;
-        if (DT.dominates(&B1, &B2))
-          outs() << B1.getName() << " dominates " << B2.getName() << "!\n";
-        else if (DT.dominates(&B2, &B1))
-          outs() << B2.getName() << " dominates " << B1.getName() << "!\n";
-      }
-    }
-
-    // Show how to check dominance relation between edge and a block.
-    BasicBlock &BBEntry = F.getEntryBlock();
-    BranchInst *TI = dyn_cast<BranchInst>(BBEntry.getTerminator());
-    if (!TI) {
-      outs() << "Unknown input!\n";
-    } else {
-      for (unsigned i = 0; i < TI->getNumSuccessors(); ++i) {
-        BasicBlock *BBNext = TI->getSuccessor(i);
-        BasicBlockEdge BBE(&BBEntry, BBNext);
-
-        for (BasicBlock &BB : F) {
-          if (DT.dominates(BBE, &BB)) {
-            outs() << "Edge (" << BBEntry.getName() << ", " << BBNext->getName()
-                  << ") dominates " << BB.getName() << "!\n";
-          }
+    PreservedAnalyses run(Function &F, FunctionAnalysisManager &FAM) {
+        DominatorTree &DT = FAM.getResult<DominatorTreeAnalysis>(F);
+        for (Function::iterator I = F.begin(), E = F.end(); I != E; ++I) {
+            for (Function::iterator I2 = I; I2 != E; ++I2) {
+                BasicBlock &B1 = *I;
+                BasicBlock &B2 = *I2;
+                if (DT.dominates(&B1, &B2)){
+                    outs() << B1.getName() << " dominates " << B2.getName() << "!\n";
+                }else if (DT.dominates(&B2, &B1)){
+                    outs() << B2.getName() << " dominates " << B1.getName() << "!\n";
+                }
+            }
         }
-      }
-    }
 
-    return PreservedAnalyses::all();
-  }
+        // Show how to check dominance relation between edge and a block.
+        BasicBlock &BBEntry = F.getEntryBlock();
+        BranchInst *TI = dyn_cast<BranchInst>(BBEntry.getTerminator());
+        if (!TI) {
+            outs() << "Unknown input!\n";
+        } else {
+            for (unsigned i = 0; i < TI->getNumSuccessors(); ++i) {
+                BasicBlock *BBNext = TI->getSuccessor(i);
+                BasicBlockEdge BBE(&BBEntry, BBNext);
+
+                for (BasicBlock &BB : F) {
+                    if (DT.dominates(BBE, &BB)) {
+                        outs() << "Edge (" << BBEntry.getName() << ", " << BBNext->getName()
+                          << ") dominates " << BB.getName() << "!\n";
+                    }
+                }
+            }
+        }
+
+        return PreservedAnalyses::all();
+    }
 };
 }
 
 extern "C" ::llvm::PassPluginLibraryInfo
 llvmGetPassPluginInfo() {
-  return {
-    LLVM_PLUGIN_API_VERSION, "PrintDominance", "v0.1",
-    [](PassBuilder &PB) {
-      PB.registerPipelineParsingCallback(
-        [](StringRef Name, FunctionPassManager &FPM,
-           ArrayRef<PassBuilder::PipelineElement>) {
-          if (Name == "print-dom") {
-            FPM.addPass(PrintDominance());
-            return true;
-          }
-          return false;
+    return {
+        LLVM_PLUGIN_API_VERSION, "PrintDominance", "v0.1",
+        [](PassBuilder &PB) {
+            PB.registerPipelineParsingCallback( [](StringRef Name, FunctionPassManager &FPM,
+                ArrayRef<PassBuilder::PipelineElement>) {
+                    if (Name == "print-dom") {
+                        FPM.addPass(PrintDominance());
+                        return true;
+                    }
+                    return false;
+                }
+            );
         }
-      );
-    }
-  };
+    };
 }
--- propinteq.cpp
+++ propinteq.cpp
@@ -9,27 +9,28 @@ using namespace std;
 namespace {
 class PropagateIntegerEquality : public PassInfoMixin<PropagateIntegerEquality> {
 public:
-  PreservedAnalyses run(Function &F, FunctionAnalysisManager &FAM) {
-    return PreservedAnalyses::all();
-  }
+    PreservedAnalyses run(Function &F, FunctionAnalysisManager &FAM) {
+
+        return PreservedAnalyses::all();
+    }
 };
 }
 
 extern "C" ::llvm::PassPluginLibraryInfo
 llvmGetPassPluginInfo() {
-  return {
-    LLVM_PLUGIN_API_VERSION, "PropagateIntegerEquality", "v0.1",
-    [](PassBuilder &PB) {
-      PB.registerPipelineParsingCallback(
-        [](StringRef Name, FunctionPassManager &FPM,
-           ArrayRef<PassBuilder::PipelineElement>) {
-          if (Name == "prop-int-eq") {
-            FPM.addPass(PropagateIntegerEquality());
-            return true;
-          }
-          return false;
+    return {
+        LLVM_PLUGIN_API_VERSION, "PropagateIntegerEquality", "v0.1",
+        [](PassBuilder &PB) {
+            PB.registerPipelineParsingCallback(
+            [](StringRef Name, FunctionPassManager &FPM,
+            ArrayRef<PassBuilder::PipelineElement>) {
+                if (Name == "prop-int-eq") {
+                    FPM.addPass(PropagateIntegerEquality());
+                    return true;
+                }
+                return false;
+            }
+            );
         }
-      );
-    }
-  };
+    };
 }
