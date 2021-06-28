0d02f3314e818524875bdea7bb7dd316a5c00dd0
hyokkkk <tahyok7123@naver.com>
2021-06-28 19:59:32 +0900

    제출파일-Revised ver.

--- propinteq.cpp
+++ propinteq.cpp
@@ -35,12 +35,9 @@ using namespace llvm::PatternMatch;
 //    ** 그 후에 replace 당할 register name("loser")과 replace를 하게 될 "winner"를 찾는다.
 //            -> 1) inst vs. inst / 2) arg vs. arg / 3) arg vs. inst 인 경우가 있다.
 //            -> BFS 순으로 inst가 "instV"에 저장되기 때문에 index로 dominance 판단 가능.
-// 4. <find condUser>
-//    condUser가 br instruction인 경우, 현재 BB를 BBEdge의 startBB로 만들어야 하므로
-//    `br i1 %cond, label %true, label %false`, 즉, %cond를 사용하는 "condUser" 찾음.
-// 5. <find loserUser>
+// 4. <find loserUser>
 //    3에서 구한 "loser"를 instruction에 사용하고 있는 user("loserUser")를 찾는다.
-// 6. <replace>
+// 5. <replace>
 //    "loserUser" 중에서 4에서 구한 startBB와 true BB 사이의 edge에게 dominate 당하는
 //    "loserUser"의 operand만 "winner"로 바꾼다.
 //        -> optimize 하려면 dummy block을 넣어야 함. printdom.cpp의
@@ -48,7 +45,6 @@ using namespace llvm::PatternMatch;
 //        -> "condUser"이 위치하는 BB(startBB)와, true BB 사이의 edge가
 //           "loserUserBB"를 dominate 하면 "winner"로 replace 가능.
 
-static vector<Value*> argV;             // vectors for function arguments.
 static vector<Value*> instV;            // vectors for instructions in function.
 static vector<BasicBlock*> BFS;         // queue for BasicBlock BFS
 static Value* winner;                   // a syntax which will replace "loser".
@@ -56,27 +52,22 @@ static Value* loser;                    // a syntax which will be replaced by "w
 
 namespace {
 class PropagateIntegerEquality : public PassInfoMixin<PropagateIntegerEquality> {
-// =========================== entry point ===================================
+
+//
+// entry point
+//
 public:
 PreservedAnalyses run(Function &F, FunctionAnalysisManager &FAM) {
-
-    // 1. <push args into vector>
-    for (Argument &Arg : F.args()){
-        Value* arg = &Arg;
-        argV.push_back(arg);
-    }
     // 1. <BB into BFS vector by BFS order>
     BFSorder(F);
     for (auto BBp : BFS){
-        for (auto &I : *BBp){
-            replaceEquality(F, FAM, &I);
-        }
+        for (auto &I : *BBp){ replaceEquality(F, FAM, &I); }
     }
     return PreservedAnalyses::all();
 }
-//============================================================================
-bool visited(BasicBlock* BBp)
-{
+
+
+bool visited(BasicBlock* BBp){
   for (BasicBlock* bp : BFS){
     if (bp != BBp){ continue; }
     return true;
@@ -84,8 +75,7 @@ bool visited(BasicBlock* BBp)
   return false;
 }
 
-void BFSorder(Function& F)
-{
+void BFSorder(Function& F){
   BasicBlock* entryBBp = &F.getEntryBlock();
   BFS.push_back(entryBBp);
 
@@ -107,140 +97,70 @@ void BFSorder(Function& F)
   }
 }
 
-//============================================================================
-// The reason why "F" and "FAM" are in param is to use 'checkDominance()' function.
 void replaceEquality(Function &F, FunctionAnalysisManager &FAM, Value *V ) {
-
     // 2. check the instruction is `%cond = icmp i32 %a, %b` or not.
+    DominatorTree &DT = FAM.getResult<DominatorTreeAnalysis>(F);
     Instruction* inst = dyn_cast<Instruction>(V);
     assert(inst);
+
     instV.push_back(inst);
-    Instruction &I = *inst;
     Value* Op0, *Op1;                   // Op0 : %a, Op1 : %b
     ICmpInst::Predicate Pred;
 
     // if not matched with '%cond = icmp eq (v1, v2)', return.
-    if (!(match(&I, m_ICmp(Pred, m_Value(Op0), m_Value(Op1))) && Pred == ICmpInst::ICMP_EQ)) {
-         return ;
-    }
-    /****** codes below are only executed when matched with `%cond = icmp eq (v1, v2)` ********/
+    if (!(match(inst, m_ICmp(Pred, m_Value(Op0), m_Value(Op1))) && 
+        Pred == ICmpInst::ICMP_EQ)) { return ; }
 
-    // 3. check Op0, Op1 are whether 'arg' or 'inst', and decide who's the "winner" and "loser"
-    decideWinnerLoser(I.getOperand(0), I.getOperand(1), F, FAM);
+    // 3. arg? inst?
+    decideWinnerLoser((*inst).getOperand(0), (*inst).getOperand(1));
 
-    // 4. find "condUser" which uses %cond in its insturction.
-    //     ex) `br i1 %cond, label %true, label %false`
-    //     Use a loop to find "condUser" for preventing erorrs,
-    //     althoguh it is guaranteed that there exists only one "condUser" in our assn.
-    for (auto itr = V->use_begin(), end = V->use_end(); itr != end;) {
-        Use &U = *itr++;
-        User *condUser = U.getUser();
+    // 4. find "loserUsers"
+    for (auto loserItr = loser->use_begin(), loserEnd = loser->use_end();
+                                                    loserItr != loserEnd;){
+        Use& loserUse = *loserItr++;
+        Instruction* targetInst = dyn_cast<Instruction>(loserUse.getUser());
+        assert(targetInst);
 
-        Instruction *brInst = dyn_cast<Instruction>(condUser);
-        assert(brInst);
+        // 5. replace "loser"s which are in "targetBB"s with "winner" only when
+        //    "targetBB" is dominated by BBEdge(entryBB, trueBB).
+        //    it works same as "dummy block"
+        BasicBlock* startBB = inst->getParent();
+        BranchInst *TI = dyn_cast<BranchInst>(startBB->getTerminator());
+        BasicBlockEdge BBE(startBB, TI->getSuccessor(0));
 
-        // "condUser" shoud be "br instruction".
-        Value* C;
-        BasicBlock* TrueBB;
-        BasicBlock* FalseBB;
-        if (!(match(brInst, m_Br(m_Value(C), m_BasicBlock(TrueBB), m_BasicBlock(FalseBB))))){
-            return ;
-        }
-
-        // 5. find "loserUsers"
-        for (auto loserItr = loser->use_begin(), loserEnd = loser->use_end();
-                                                        loserItr != loserEnd;){
-            Use& loserUse = *loserItr++;
-            User* loserUser = loserUse.getUser();
-            Instruction* targetInst = dyn_cast<Instruction>(loserUser);
-            assert(targetInst);
-
-            // 6. replace "loser"s which are in "targetBB"s with "winner" only when
-            //    "targetBB" is dominated by BBEdge(entryBB, trueBB).
-            //    it works same as "dummy block"
-            BasicBlock* targetBB = targetInst->getParent();
-            if (checkBBEDominance(*(inst->getParent()), *targetBB, F, FAM)){
-                // loser가 use되는 곳을 winner로 set
-                loserUse.set(winner);
-            }
-        }
+        // loser가 use되는 곳을 winner로 set
+        if (DT.dominates(BBE, targetInst->getParent())){ loserUse.set(winner); }
     }
 }
 
-//============================================================================
-void decideWinnerLoser(Value* Op0, Value* Op1, Function& F, FunctionAnalysisManager& FAM){
+//
+// Parse the values
+//
+void decideWinnerLoser(Value* X, Value* Y){
+    auto isXArg = dyn_cast<Argument>(X);
+    auto isYArg = dyn_cast<Argument>(Y);
+    int NoX = -1, NoY = -1;
 
-        // -1 : instruction (not in the argV)
-        // 0, 1, 2... : index in argV
-        int op0argV = -1;
-        int op1argV = -1;
-        for (int i = 0; i < argV.size(); i++){
-            // compare by using names.
-            if ((*argV[i]).getName().equals((*Op0).getName())){
-                op0argV = i;
-            }else if((*argV[i]).getName().equals((*Op1).getName())){
-                op1argV = i;
-            }
+    if (isXArg && isYArg){
+        loser = (NoX = isXArg->getArgNo()) > (NoY = isYArg->getArgNo()) ? X : Y;
+        winner = NoX < NoY ? X : Y;
+    }else if (!isXArg && !isYArg){
+        // find the index each instruction is in.
+        for (int i = 0; i < instV.size(); i++){
+            if ((*instV[i]).getName().equals((*X).getName())){ NoX = i; }
+            else if((*instV[i]).getName().equals((*Y).getName())){ NoY = i; }
         }
-        // (1) inst vs. inst : first executed, become winner.
-        //     - compare instV index. (first come, first executed.)
-        if (op0argV == -1 && op1argV == -1){
-            Instruction *instOp0, *instOp1;
-            BasicBlock *op0BB, *op1BB;
-            int op0instV = -1, op1instV = -1;
-
-            // find BBs where each instruction is.
-            for (int i = 0; i < instV.size(); i++){
-                if ((*instV[i]).getName().equals((*Op0).getName())){
-                    op0instV= i;
-                }else if((*instV[i]).getName().equals((*Op1).getName())){
-                    op1instV= i;
-                }
-            }
-                loser = op0instV > op1instV ? Op0 : Op1;
-                winner = op0instV > op1instV ? Op1 : Op0;
-
-        // (2) arg vs. arg : first defined, become winner. @f(i32 %y, i32 %x) -> %y wins.
-        }else if (op0argV != -1 && op1argV != -1){
-            loser = op0argV > op1argV ? Op0 : Op1;
-            winner = op0argV > op1argV ? Op1 : Op0;
-        // (3) arg vs. inst : arg always wins.
-        }else {
-            loser = op0argV == -1 ? Op0 : Op1;
-            winner = op0argV == -1 ? Op1 : Op0;
-        }
-}
-
-//============================================================================
-bool checkBBEDominance(BasicBlock& startBB, BasicBlock& targetBB,
-                    Function& F, FunctionAnalysisManager& FAM)
-{
-    DominatorTree &DT = FAM.getResult<DominatorTreeAnalysis>(F);
-    BranchInst *TI = dyn_cast<BranchInst>(startBB.getTerminator());
-
-    // br i1 %cond, label %successor(0), label %successor(1)
-    BasicBlock* destBB = TI->getSuccessor(0);
-    BasicBlockEdge BBE(&startBB, destBB);
-
-    if (DT.dominates(BBE, &targetBB)){ return true; }
-    return false;
-}
-//============================================================================
-int checkInstDominance(BasicBlock& B0, BasicBlock& B1, Function& F, FunctionAnalysisManager& FAM){
-    DominatorTree &DT = FAM.getResult<DominatorTreeAnalysis>(F);
-    if (DT.dominates(&B0, &B1)){
-        // [param index] BB dominates the other BB.
-        return 0;
-    }else if (DT.dominates(&B1, &B0)){
-        return 1;
+        winner = NoX > NoY ? Y : X;
+        loser = NoX > NoY ? X : Y;
+    }else {
+        winner = isXArg ? X : Y;
+        loser = isXArg ? Y : X;
     }
-    // no dominant relation
-    return -1;
 }
 };
 }
 
-//============================================================================
+
 extern "C" ::llvm::PassPluginLibraryInfo
 llvmGetPassPluginInfo() {
     return {
